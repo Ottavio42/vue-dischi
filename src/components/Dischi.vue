31860aa6a37deff2a6e4908b5c3cccf896c97f93
Ottavio42 <ottaviog88@gmail.com>
2021-10-04 09:25:07 +0200

    dischi

---
+++ src/components/Dischi.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container dischi-cont" >
+       <div class="disco-cont" v-for="(elm, index) in dischi" :key="index">
+            <Album :info="elm" />
+      </div>
+  </div>
+</template>
+
+<script>
+import Disco from "./Disco.vue";
+import axios from "axios";
+export default {
+    name: "Dischi",
+    components: {
+        Disco
+  },
+  data(){
+      return {
+          dischi : []
+      }
+  },
+  created(){
+      axios.get("https://flynn.boolean.careers/exercises/api/array/music")
+        .then((res) =>{
+            this.dischi = res.data.response;
+        }
+        );
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dischi-cont{
+    display: flex;
+    flex-wrap: wrap;
+    gap: .9375rem 1.5625rem;
+    .disco-cont{
+        width: calc(100% / 5 - 1.5625rem);
+    }
+}
+</style>
---
+++ src/components/Disco.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="disco">
+      <img :src="info.poster" alt="info.title">
+      <div class="title">{{info.title}} </div>
+      <p class="author">{{info.author}}</p>
+      <p class="year">{{info.year}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Disco",
+  props:{ 
+      info: Object
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/style/variables.scss";
+    .disco{
+        background-color: $mainColor;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap:.625rem;
+        height: 19rem;
+        text-align: center;
+        img{
+            margin-top: 1.25rem;
+            width: 7rem;
+            height:7rem;
+        }
+        .title{
+            color:white;
+            font-weight: bold;
+        }
+        p{
+            color: $textColor;
+        }
+    }
+</style>
